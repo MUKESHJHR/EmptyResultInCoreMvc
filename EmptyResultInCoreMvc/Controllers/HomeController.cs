b9f9bc6b2dc29735babc20befc6e85ebd9b818ec
mukesh kumar <mukesh.1006jhr@outlook.com>
2023-10-20 05:51:04 +0530

    Add project files.

---
+++ EmptyResultInCoreMvc/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using EmptyResultInCoreMvc.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EmptyResultInCoreMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //public IActionResult ProcessRequest()
+        //{
+        //    // Perform some processing
+
+        //    // Return an empty HTTP response
+
+        //    return new EmptyResult();
+
+        //}
+
+        public IActionResult DeleteResource()
+        {
+            // Delete the resource
+            // Return a 204 No Content response with an EmptyResult
+            return NoContent();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ EmptyResultInCoreMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EmptyResultInCoreMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
